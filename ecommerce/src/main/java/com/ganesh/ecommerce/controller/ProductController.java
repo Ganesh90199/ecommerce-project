72dbb6b6aefd2a972f27f606d6de1999c5c37e33
Ganesh90199 <ganeshganesh99720@gmail.com>
2026-04-13 13:05:19 +0530

    Added Product API with GET and POST operations using Spring Boot and MySQL

--- ecommerce/src/main/java/com/ganesh/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/ganesh/ecommerce/controller/ProductController.java
@@ -1,13 +1,28 @@
 package com.ganesh.ecommerce.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.ganesh.ecommerce.model.Product;
+import com.ganesh.ecommerce.repository.ProductRepository;
 
 @RestController
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    // GET all products
     @GetMapping("/products")
-    public String getProducts() {
-        return "Product API is working";
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    // POST new product
+    @PostMapping("/products")
+    public Product addProduct(@RequestBody Product product) {
+        return productRepository.save(product);
     }
 }
