f1b66dbb243327baaf8bc52f5052a90b4f272d9c
Ganesh90199 <ganeshganesh99720@gmail.com>
2026-06-08 19:49:13 +0530

    Implemented cart module with JWT security

--- src/main/java/com/ganesh/ecommerce/controller/CartController.java
+++ src/main/java/com/ganesh/ecommerce/controller/CartController.java
@@ -1,12 +1,23 @@
 package com.ganesh.ecommerce.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ganesh.ecommerce.config.JwtUtil;
 import com.ganesh.ecommerce.model.Cart;
+import com.ganesh.ecommerce.model.User;
+import com.ganesh.ecommerce.repository.UserRepository;
 import com.ganesh.ecommerce.service.CartService;
 
-import java.util.List;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -15,22 +26,53 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
-    // Add to cart
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Add to Cart
     @PostMapping
-    public Cart addToCart(@RequestBody Cart cart) {
+    public Cart addToCart(@RequestBody Cart cart,
+                          HttpServletRequest request) {
+
+        String token =
+                request.getHeader("Authorization")
+                       .substring(7);
+
+        String email = jwtUtil.extractEmail(token);
+
+        User user =
+                userRepository.findByEmail(email).get();
+
+        cart.setUserId(user.getId());
+
         return cartService.addToCart(cart);
     }
 
-    // Get cart by user
-    @GetMapping("/{userId}")
-    public List<Cart> getCart(@PathVariable int userId) {
-        return cartService.getCartByUser(userId);
+    // View My Cart
+    @GetMapping("/my")
+    public List<Cart> getMyCart(HttpServletRequest request) {
+
+        String token =
+                request.getHeader("Authorization")
+                       .substring(7);
+
+        String email = jwtUtil.extractEmail(token);
+
+        User user =
+                userRepository.findByEmail(email).get();
+
+        return cartService.getCartByUser(user.getId());
     }
 
-    // Delete item
+    // Remove Item
     @DeleteMapping("/{id}")
     public String deleteCart(@PathVariable int id) {
+
         cartService.deleteCartItem(id);
+
         return "Item removed";
     }
 }
